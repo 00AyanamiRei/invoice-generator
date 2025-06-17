5fefa489fff7492f40dd08e717d20fe75020e1ae
Maksym Kozlov <118828568+00AyanamiRei@users.noreply.github.com>
2025-06-17 19:12:42 +0430

    Update SpringSecurityConfig.java

--- src/main/java/com/makara/invoicegenerator/SpringSecurityConfig.java
+++ src/main/java/com/makara/invoicegenerator/SpringSecurityConfig.java
@@ -14,7 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
-@EnableGlobalMethodSecurity(securedEnabled = true) //habilitar protección de rutas por anotacionea
+@EnableGlobalMethodSecurity(securedEnabled = true)
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig {
@@ -31,44 +31,17 @@ public class SpringSecurityConfig {
 	@Autowired
 	private JpaUserDetailsService userDetailsServiceJpa;
 	
-//	@Bean //configuration global
-//	public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
-//
-//		List<UserDetails> userDetailsList = new ArrayList<>();
-//		userDetailsList.add(User.withUsername("admin").password(this.passwordEncoder.encode("password"))
-//				.roles("ADMIN", "USER").build());
-//		userDetailsList.add(User.withUsername("andres").password(this.passwordEncoder.encode("password"))
-//				.roles("USER").build());
-//
-//		return new InMemoryUserDetailsManager(userDetailsList);
-//	}
-	
-	//con jpa y las clases entity
 	@Autowired
 	public void configurerGlobal(AuthenticationManagerBuilder build) throws Exception{
 		build.userDetailsService(userDetailsServiceJpa).passwordEncoder(passwordEncoder);
 	}
-	
-	/*
-	@Autowired //con jdbc
-	public void configurerGlobal(AuthenticationManagerBuilder build) throws Exception{
-		build.jdbcAuthentication()
-		.dataSource(dataSource)
-		.passwordEncoder(passwordEncoder)
-		.usersByUsernameQuery("SELECT username, password, enabled FROM users WHERE username=?")
-		.authoritiesByUsernameQuery("SELECT u.username, a.authority FROM authorities a INNER JOIN users u ON (a.user_id=u.id) WHERE u.username=?");
-	}*/
 
 
-	@Bean //autorización
+
+	@Bean 
 	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/", "/css/**", "/js/**", "/images/**", "/list", "/locale", "/register")
 				.permitAll()
-				/*.antMatchers("/ver/**").hasAnyRole("USER")
-                .antMatchers("/uploads/**").hasAnyRole("USER")
-                .antMatchers("/form/**").hasAnyRole("ADMIN")
-                .antMatchers("/eliminar/**").hasAnyRole("ADMIN")
-                .antMatchers("/factura/**").hasAnyRole("ADMIN")*/
 				.antMatchers("/admin/**").hasRole("ADMIN")
 				.anyRequest().authenticated()
 				.and()
